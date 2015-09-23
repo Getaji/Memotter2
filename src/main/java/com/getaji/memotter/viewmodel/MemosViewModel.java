3f1d5feb4a3c401e182aed1d5e82ad1ad67a2213
Getaji <t.tanaka.onion@gmail.com>
2015-09-23 12:42:49 +0900

    MemosViewModelに各入力フィールドのプロパティを追加

--- src/main/java/com/getaji/memotter/viewmodel/MemosViewModel.java
+++ src/main/java/com/getaji/memotter/viewmodel/MemosViewModel.java
@@ -6,6 +6,9 @@ import com.getaji.memotter.model.ViewModelPair;
 import com.getaji.memotter.view.MemosView;
 import javafx.beans.property.Property;
 import javafx.beans.property.SimpleObjectProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+import lombok.Getter;
 import lombok.val;
 
 /**
@@ -18,11 +21,37 @@ public class MemosViewModel {
     }
 
     private final Property<Memo> selectedMemo = new SimpleObjectProperty<>();
+    private final StringProperty title = new SimpleStringProperty();
+    private final StringProperty description = new SimpleStringProperty();
+    private final StringProperty tags = new SimpleStringProperty();
 
     // Getter
-
     public Memo getSelectedMemo() {
         return selectedMemo.getValue();
+    }
+
+    public String getTitle() {
+        return title.get();
+    }
+
+    public StringProperty titleProperty() {
+        return title;
+    }
+
+    public String getDescription() {
+        return description.get();
+    }
+
+    public StringProperty descriptionProperty() {
+        return description;
+    }
+
+    public String getTags() {
+        return tags.get();
+    }
+
+    public StringProperty tagsProperty() {
+        return tags;
     }
 
     public Property<Memo> selectedMemoProperty() {
